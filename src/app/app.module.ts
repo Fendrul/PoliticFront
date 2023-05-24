baceddaf3d62cd92a0a861518dce886cfd764fd7
Simon Strykers <simon.strykers@gmail.com>
2023-05-24 14:43:09 +0200

    Implémentation d'Angular matérial + création de palettes personnalisées

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BannerComponent } from './modules/components/banner/banner.component';
-import { ToolbarComponent } from './modules/components/banner/toolbar/toolbar.component';
-import { SearchBarComponent } from './modules/components/banner/toolbar/search-bar/search-bar.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BannerComponent} from './modules/components/banner/banner.component';
+import {ToolbarComponent} from './modules/components/banner/toolbar/toolbar.component';
+import {SearchBarComponent} from './modules/components/banner/toolbar/search-bar/search-bar.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { ArgumentPanelComponent } from './modules/components/body/argument-panel/argument-panel.component';
-import { FilterCheckboxComponent } from './modules/components/banner/toolbar/search-bar/filter-checkbox/filter-checkbox.component';
+import {ArgumentPanelComponent} from './modules/components/body/argument-panel/argument-panel.component';
+import {
+  FilterCheckboxComponent
+} from './modules/components/banner/toolbar/search-bar/filter-checkbox/filter-checkbox.component';
 import {DropdownModule} from "primeng/dropdown";
 import {CheckboxModule} from "primeng/checkbox";
 import {MultiSelectModule} from "primeng/multiselect";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {HttpClientModule} from "@angular/common/http";
-import { CategoryListComponent } from './modules/components/body/category-list/category-list.component';
+import {CategoryListComponent} from './modules/components/body/category-list/category-list.component';
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
 
 @NgModule({
   declarations: [
@@ -35,9 +38,11 @@ import { CategoryListComponent } from './modules/components/body/category-list/c
     CheckboxModule,
     MultiSelectModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
